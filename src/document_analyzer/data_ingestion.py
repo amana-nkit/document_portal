d7a6c35a8dfb37303a5bcb6cbbf7432c76d6ddc4
Aman Ankit <amanankit2021@outlook.com>
2025-08-08 07:40:46 +0530

    v1.2

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     from pathlib import Path
     from io import BytesIO
     
-    pdf_path=r"C:\\Users\\sunny\\document_portal\\data\\document_analysis\\sample.pdf"
+    pdf_path=r"C:\\AI Agent\\LLM Project\\document_portal-main\\data\\document_analysis\\sample.pdf"
     class DummnyFile:
         def __init__(self,file_path):
             self.name = Path(file_path).name
